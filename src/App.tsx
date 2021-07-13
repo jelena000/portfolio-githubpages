f9b3d90e1d0a1af8fcebbdd93b913e6e831c51f0
AleksaRadovanovic <lekaaa111.55@gmail.com>
2021-07-13 05:42:23 +0200

    completed3

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="App">
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Header />
           <Route exact path="/" component={Main}></Route>
           <Route path="/work/:id" component={Work}/>
